36e73a12f031a0cb48adfbfaed737f678ec28d9a
Izhan <babuizhan1234@gmail.com>
2022-05-10 08:11:00 +0530

    added import for gameRouter

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname + "/.env" });
-import express from "express";
-import cors from "cors";
-import noteRouter from "./controller/note";
-import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
+import express from 'express';
+import cors from 'cors';
+import gameRouter from './controller/game';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
@@ -12,18 +12,18 @@ app.use(express.json());
 mongoose
   .connect(`${process.env.MONGO_URL}`)
   .then(() => {
-    console.log("connected to MongoDB database");
+    console.log('connected to MongoDB database');
   })
   .catch((err) => {
-    console.log("error connecting to MongoDB: ", err.message);
+    console.log('error connecting to MongoDB: ', err.message);
   });
 
 const PORT = 3001;
 
-app.use("/api/game", gameRouter);
+app.use('/api/game', gameRouter);
 
-app.get("/api/ping", (_req, res) => {
-  res.send("pong");
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
 });
 
 app.listen(PORT, () => {
